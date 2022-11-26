2473adad20e9ad41f12875b857c482abf4159f0b
asadfgglie <ac121878@outlook.com>
2022-11-26 22:26:55 +0800

    v1.1.0 add Screen broad change FPS to 60

--- src/main/java/ckcsc/asadfgglie/main/Main.java
+++ src/main/java/ckcsc/asadfgglie/main/Main.java
@@ -4,19 +4,30 @@ import ckcsc.asadfgglie.pet.Pet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Main {
-    public static final int FPS = 30;
+    public static final int FPS = 60;
     public static final int MAX_SIZE = 200;
+    public static final int SCREEN_SIZE_X = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    public static final int SCREEN_SIZE_Y = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() - Toolkit.getDefaultToolkit().getScreenInsets(new JFrame().getGraphicsConfiguration()).bottom;
+
+    public static final double GRAVITY = 30.0 / FPS;
+
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main (String[] args) throws Exception {
-        Pet[] pets = Pet.loadPets(args[0], logger);
+        ArrayList<Pet> pets = Pet.loadPets(args[0], logger);
 
+        logger.info("Screen size: " + SCREEN_SIZE_X + ", " + SCREEN_SIZE_Y);
         logger.info("Resources have loaded.");
 
         for(Pet pet : pets){
             pet.start();
+            logger.info("Generate " + pet.name);
             Thread.sleep(1000);
         }
     }
